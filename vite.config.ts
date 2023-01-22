283769b076f0f0e3234afb0169bd86248915308e
Maria Bracho <84558868+MariaBracho@users.noreply.github.com>
2023-01-22 00:32:30 -0400

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://mariabracho.github.io/rick-and-morty/",
+  base:"/rick-and-morty/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
